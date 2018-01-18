78f4d538f85d8721c45ff4c665f2c3477f591868
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2018-01-18 09:29:29 -0500

    selectionSort

--- SortingLab/src/sortingAlgorithms.java
+++ SortingLab/src/sortingAlgorithms.java
@@ -2,19 +2,46 @@
 public class sortingAlgorithms {
 
 	
-	public static void bubbleSort(int[] elements)
+	public static void bubbleSort(String[] list1)
 	{
 		int swapCount = 0;
 		while (swapCount<0)
 		{
 			swapCount = 0;
 			
-			for (int i = 0; i<elements.length-1; i++)
+			for (int i = 0; i<list1.length-1; i++)
 			{
-				if (elements[i]<elements[i+1])
+				if (list1[i].compareTo(list1[i+1])>0)
 				{
-					swapMethods.swap(elements, i, i+1);
+					swapMethods.swap(list1, i, i+1);
 					swapCount++;
+				}
+			}
+		}
+	}
+	
+	
+	public static void insertionSort(int[] list1)
+	{	
+			
+	}
+	
+	// 6    2    8    5
+	public static void selectionSort(double[] list1)
+	{
+		int swapCount = 0;
+		while (swapCount<0)
+		{
+			swapCount = 0;
+			
+			for (int i = 0; i<list1.length-1;)
+			{
+				if (list1[0]>list1[i])
+				{
+					swapMethods.swap(list1, 0, i);
+					swapCount++;
+					i++;
+					list1[0] = list1[i];
 				}
 			}
 		}
