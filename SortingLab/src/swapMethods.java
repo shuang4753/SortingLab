d1097b15bea9d2a5328079e89b69109a41b86920
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2018-01-18 08:57:46 -0500

    asd

---
+++ SortingLab/src/swapMethods.java
@@ -0,0 +1,24 @@
+//
+public class swapMethods {
+
+	public static void swap(int[] elements, int index1, int index2)
+	{
+		 int temp=elements[index1];
+		 elements[index1]= elements[index2];
+		 elements[index2]=temp;
+	}
+	
+	public static void swap(double[] elements, int index1, int index2)
+	{
+		 double temp=elements[index1];
+		 elements[index1]= elements[index2];
+		 elements[index2]=temp;
+	}
+	
+	public static void swap(String[] elements, int index1, int index2)
+	{
+		 String temp=elements[index1];
+		 elements[index1].equals(elements[index2]);
+		 elements[index2].equals(temp);
+	}
+}
