739dfaa09808ed706519237d262acb1b4df172d4
huang <huang@DESKTOP-LDKDTR6.home>
2018-02-16 02:31:17 -0500

    polish

--- SortingLab/src/Runner.java
+++ SortingLab/src/Runner.java
@@ -8,56 +8,56 @@ public class Runner {
 	public static void main(String[] args)
 	{
 		//Testing arrays
-		int[] insert1 = {1,4,4,5,2,4,3,17,0};
-		double[] selection2 = {1.1, 4.1, 4.2, 5.0, 2.0, 4.0, 17.0, 0.0};
-		String[] bubble3 = {"zebra", "tortilla", "abba", "foo", "bar", "aba"};
-		String[] merge1 = {"gate", "donut", "apple", "as", "think", "lose"};
-		int[] quick1 = {1, 7, 9, 11, 19, 2, 5, 6, 11, 17, 8, 13, 14, 15, 20, 1, 3, 7, 14, 18};
+		int[] insertTest = {1,4,4,5,2,4,3,17,0};
+		double[] selectionTest = {1.1, 4.1, 4.2, 5.0, 2.0, 4.0, 17.0, 0.0};
+		String[] bubbleTest = {"zebra", "tortilla", "abba", "foo", "bar", "aba"};
+		String[] mergeTest = {"gate","boo", "donut", "zebra", "apple","chick", "as", "tortilla", "think", "lose"};
+		int[] quickTest = {1, 7, 9, 11, 19, 2, 5, 6, 11, 17, 8, 13, 14, 15, 20, 1, 3, 7, 14, 18};
 				
 	
 		//Insertion Sort Test
 		long start= System.nanoTime();
-		sortingAlgorithms.insertionSort(insert1);
+		sortingAlgorithms.insertionSort(insertTest);
 		long end = System.nanoTime();
 		long time = end - start;
 		System.out.println("InsertionSort took: " + time + "nanoseconds");
-		System.out.println(Arrays.toString(insert1) + "\n");
+		System.out.println(Arrays.toString(insertTest) + "\n");
 		
 		
 		//Selection Sort Test
 		start = System.nanoTime();
-		sortingAlgorithms.selectionSort(selection2);
+		sortingAlgorithms.selectionSort(selectionTest);
 		end = System.nanoTime();
 		time = end - start;
 		System.out.println("SelectionSort took: " + time + "nanoseconds");
-		System.out.println(Arrays.toString(selection2) + "\n");
+		System.out.println(Arrays.toString(selectionTest) + "\n");
 		
 		
 		//Bubble Sort Test
 		start = System.nanoTime();
-		sortingAlgorithms.bubbleSort(bubble3);
+		sortingAlgorithms.bubbleSort(bubbleTest);
 		end = System.nanoTime();
 		time = end - start;
 		System.out.println("BubbleSort took: " + time + "nanoseconds");
-		System.out.println(Arrays.toString(bubble3) + "\n");
+		System.out.println(Arrays.toString(bubbleTest) + "\n");
 		
 		
 		//Merge Test
 		start = System.nanoTime();
-		String[] merge2 = sortingAlgorithms.mergeSort(merge1);
+		String[] mergeFinal = sortingAlgorithms.mergeSort(mergeTest);
 		end = System.nanoTime();
 		time = end - start;
 		System.out.println("Merge took: " + time + "nanoseconds");
-		System.out.println(Arrays.toString(merge2) + "\n");
+		System.out.println(Arrays.toString(mergeFinal) + "\n");
 		
 		
 		//QuickSort Test
 		start = System.nanoTime();
-		sortingAlgorithms.quickSort(quick1, 0, quick1.length-1);
+		sortingAlgorithms.quickSort(quickTest, 0, quickTest.length-1);
 		end = System.nanoTime();
 		time = end - start;
 		System.out.println("QuickSort took: " + time + "nanoseconds");
-		System.out.println(Arrays.toString(quick1) + "\n");
+		System.out.println(Arrays.toString(quickTest) + "\n");
 		
 	}
 	
